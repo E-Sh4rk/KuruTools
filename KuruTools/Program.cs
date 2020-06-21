8d25baf0e026b03ef1d035fd511c76dd68661a81
Mickael Laurent <esh4rk@gmail.com>
2020-06-20 21:20:27 -0500

    add transparency to tiles

--- KuruTools/Program.cs
+++ KuruTools/Program.cs
@@ -19,15 +19,10 @@ namespace KuruTools
         static void Main(string input = "input.gba", string output = "output.gba", string workspace = "levels",
             bool relocate = true, string extractTiles = null, bool debug = false)
         {
-            // TODO: option to treat first color as transparency for sprite extraction?
             Console.WriteLine("=== Kuru Kuru Kururin Tools ===");
             Console.WriteLine("");
             File.Copy(input, output, true);
             Levels levels = new Levels(output);
-
-            // Some tests and experiments...
-            /*Levels.RawMapData raw_lvl = levels.extractLevelData(new Levels.LevelIdentifier(Levels.World.TRAINING, 0));
-            File.WriteAllBytes("training1_compressed.bin", raw_lvl.CompressedData);*/
 
             // Print all level infos
             if (debug)
@@ -73,7 +68,7 @@ namespace KuruTools
                                 for (int j = 0; j < palette.Colors.Length; j++)
                                 {
                                     string filename_png = string.Format("{0}.{1}.{2:D2}.png", Levels.LevelIdentifier.WorldShortName(w), type, j);
-                                    Tiles.PreviewOfTilesData(d, palette.Colors[j]).Save(Path.Combine(extractTiles, filename_png));
+                                    Tiles.PreviewOfTilesData(d, palette.Colors[j], i != 1).Save(Path.Combine(extractTiles, filename_png));
                                 }
                             }
                         }
@@ -133,26 +128,6 @@ namespace KuruTools
                 if (levels.AlterLevelData(level, p, g, b, m, relocate))
                     Console.WriteLine("Changes detected in " + level.ToString() + ". The ROM has been updated.");
             }
-
-            // Some tests and experiments...
-            /*Levels.RawMapData raw_gl = levels.extractLevelData(new Levels.LevelIdentifier(Levels.World.GRASS, 0));
-            File.WriteAllBytes("grassland1_raw.bin", raw_gl.RawData);
-            Map m = new Map(raw_gl.RawData);
-            File.WriteAllText("grassland1.txt", m.toString());
-            File.WriteAllBytes("grassland1_raw_2.bin", m.toByteData());
-            File.WriteAllBytes("grassland1_compressed.bin", raw_gl.CompressedData);
-            FileStream f = File.OpenWrite("grassland1_compressed_2.bin");
-            LzCompression.compress(f, raw_gl.RawData);
-            f.Close();*/
-            /*Map lvl = Map.parse(File.ReadAllLines("levels/training_1.txt"));
-            FileStream f = File.OpenWrite("training1_compressed_2.bin");
-            LzCompression.compress(f, lvl.toByteData());
-            f.Close();
-            f = File.OpenRead("training1_compressed_2.bin");
-            byte[] raw = LzCompression.decompress(f, 0x4004);
-            f.Close();
-            lvl = new Map(raw);
-            File.WriteAllText("training1_reparsed.txt", lvl.toString());*/
 
             Console.WriteLine("All tasks terminated.");
             levels.Dispose();
